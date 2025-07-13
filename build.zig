7d9d8e5c6c658084d29c8bdbf2ce480ccb78cd23
Olle Lukowski <olle@lukowski.dev>
2025-07-13 12:27:02 +0200

    build: ensure the module is public

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.createModule(.{
+    const mod = b.addModule("otter", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
